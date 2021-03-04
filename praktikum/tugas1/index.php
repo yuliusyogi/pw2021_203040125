e50f7b1e11d0174a82d3e48c7ea4f818b6e4f548
yuliusyogi <76626242+yuliusyogi@users.noreply.github.com>
2021-03-04 23:15:38 +0700

    Add files via upload

---
+++ praktikum/tugas1/index.php
@@ -0,0 +1,48 @@
+<<<<<<< HEAD
+<?php
+/*
+    Yulius Yogi Yuwono
+    203040125
+    Jumat,13.00
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <table border="2" cellpadding="20" cellspacing="5">
+    <table border="2" cellpadding="20" cellspacing="5" align="center">
+        <?php for ($i = 1; $i <= 6; $i++) : ?>
+            <tr>
+=======
+<?php
+/*
+    Yulius Yogi Yuwono
+    203040125
+    Jumat,13.00
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <table border="2" cellpadding="20" cellspacing="5">
+    <table border="2" cellpadding="20" cellspacing="5" align="center">
+        <?php for ($i = 1; $i <= 6; $i++) : ?>
+            <tr>
+>>>>>>> f28fa6f081f2ff5cdb7cd428e14b7e4f33391a7e
+                <?php for ($j = 1; $j <= 6; $j++) : ?>
