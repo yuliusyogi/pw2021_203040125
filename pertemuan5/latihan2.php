a9374f58d01a6880d3e10c9d3c3e7423065316c7
yuliusyogi <76626242+yuliusyogi@users.noreply.github.com>
2021-03-25 16:34:35 +0700

    Update latihan2.php

--- pertemuan5/latihan2.php
+++ pertemuan5/latihan2.php
@@ -8,44 +8,41 @@
 
 // pengulangan pada array
 // for / foreach
-$angka = [3,2,15,20,11,88,1,45];
+$angka = [3,2,15,20,11,77,89];
 
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Latihan2</title>
-    <style>
-        .kotak {
-            width: 50px;
-            height: 50px;
-            background-color: salmon;
-            text-align: center;
-            line-height: 50px;
-            margin: 3px;
-            float: left;
-        }
-        .clear {clear: both;}
-    </style>
+	<title>Latihan 2</title>
+	<style>
+		.kotak {
+			width: 50px;
+			height: 50px;
+			background-color: salmon;
+			text-align: center;
+			line-height: 50px;
+			margin: 3px;
+			float: left;
+		}
+		.clear {clear: both;}
+	</style>
 </head>
 <body>
-    <?php for($i =0; $in < count($angka); $i++) { ?>
-            <div class="kotak"><?php echo $a; ?></div>
-    <?php } ?>
+	<?php for($i = 0; $i < count($angka); $i++) { ?>
+		<div class="kotak"><?php echo $angka[$i]; ?></div>
+	<?php } ?>
 
-            <div class="clear"></div>
+		<div class="clear"></div>
 
-    <?php foreach($angka as $a) { ?>
-            <div class="kotak"><?php echo $a; ?></div>
-    <?php } ?>
+	<?php foreach($angka as $a) { ?>
+		<div class="kotak"><?php echo $a; ?></div>
+	<?php } ?>
+		<div class="clear"></div>
 
-            <div class="clear"></div>
-
-    <?php foreach($angka as $a) : ?>
-            <div class="kotak"><?=$a; ?></div>
-    <?php endforeach; ?>
-
-
+	<?php foreach($angka as $a) : ?>
+		<div class="kotak"><?= $a; ?></div>
+	<?php endforeach; ?>
 </body>
 </html>
